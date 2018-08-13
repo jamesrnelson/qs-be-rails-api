9ae652ee81f2186ca980c33bdff0337f3988e925
JNelson <nelson.jimmy@gmail.com>
2018-08-13 14:26:47 -0600

    Moves individual food test to appropriate file

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -7,17 +7,4 @@ describe Food do
     expect(food1.name).to eq('Banana')
     expect(food1.calories).to eq(150)
   end
-
-  it 'can show an individual food' do
-    food1 = Food.create(name: 'Steak', calories: 600)
-    id = food1.id
-
-    get "/api/v1/foods/#{id}"
-    expect(response).to be_success
-    food = JSON.parse(response.body)
-
-    expect(food['id']).to eq(id)
-    expect(food['name']).to eq(food1.name)
-    expect(food['calories']).to eq(food1.calories)
-  end
 end
