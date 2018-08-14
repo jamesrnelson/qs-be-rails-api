1ceaba068fb8736e2d54fbb4845857d43c1d9896
JNelson <nelson.jimmy@gmail.com>
2018-08-14 13:16:39 -0600

    Adds happy path test for posting to meal foods endpoint

---
+++ spec/requests/api/v1/meal_foods/meal_foods_post_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'post /api/v1/meals/:meal_id/foods/:id' do
+  it 'should reutn a status code of 201 with a specific message' do
+    meal1 = Meal.create(name: 'Breakfast')
+    food1 = Food.create(name: 'Banana', calories: 150)
+
+    message = { "message": "Successfully added #{food1.name} to #{meal1.name}" }
+
+    post "/api/v1/meals/#{meal1.id}/foods/#{food1.id}"
+
+    expect(response.status).to eq(201)
+    successful_response = JSON.parse(response.body, symbolize_names: true)
+    expect(successful_response).to eq(message)
+
+    get "/api/v1/meals/#{meal1.id}/foods"
+
+    expected = {
+      'id': meal1.id,
+      'name': meal1.name,
+      'foods': [
+        {
+          'id': food1.id,
+          'name': food1.name,
+          'calories': food1.calories
+        }
+      ]
+    }
+
+    meal = JSON.parse(response.body, symbolize_names: true)
+
+    expect(meal).to eq(expected)
+  end
+end
