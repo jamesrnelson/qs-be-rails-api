80d20d01fdd53c1afb2b20215742cd3e3b088e75
JNelson <nelson.jimmy@gmail.com>
2018-08-14 09:27:42 -0600

    Adds route and controller action for finding one meal

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :foods, only: %i[index show create update destroy]
       get '/meals', to: 'meals#index'
+      get '/meals/:id/foods', to: 'meals#show'
     end
   end
 end
