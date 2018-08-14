404988ea1881eb079da6f611af28c64de902b760
JNelson <nelson.jimmy@gmail.com>
2018-08-14 14:26:13 -0600

    Adds happy path test for deleting a food item from a meal

---
+++ spec/requests/api/v1/meal_foods/meal_foods_delete_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'delete /api/v1/meals/:meal_id/foods/:id' do
+  it 'should return a status code of 201 with a specific message' do
+    meal1 = Meal.create(name: 'Breakfast')
+    food1 = Food.create(name: 'Banana', calories: 150)
+    MealFood.create(meal_id: meal1.id, food_id: food1.id)
+
+    expected = {
+      'id': meal1.id,
+      'name': meal1.name,
+      'foods': [
+        {
+          'id': food1.id,
+          'name': food1.name,
+          'calories': food1.calories
+        }
+      ]
+    }
+
+    get "/api/v1/meals/#{meal1.id}/foods"
+
+    meal = JSON.parse(response.body, symbolize_names: true)
+    expect(meal).to eq(expected)
+
+    message = { 'message': "Successfully removed #{food1.name} from #{meal1.name}" }
+
+    delete "/api/v1/meals/#{meal1.id}/foods/#{food1.id}"
+
+    expect(response.status).to eq(201)
+    successful_response = JSON.parse(response.body, symbolize_names: true)
+    expect(successful_response).to eq(message)
+
+    get "/api/v1/meals/#{meal1.id}/foods"
+
+    new_expectation = {
+      'id': meal1.id,
+      'name': meal1.name,
+      'foods': []
+    }
+
+    new_meal = JSON.parse(response.body, symbolize_names: true)
+    expect(new_meal).to eq(new_expectation)
+  end
+end
