56d320b14d74e015122a3c7d290f5723ac7f79b4
JNelson <nelson.jimmy@gmail.com>
2018-08-14 14:42:48 -0600

    Adds sad path tests for when a delete request references nonexistent food or meal.

--- spec/requests/api/v1/meal_foods/meal_foods_delete_spec.rb
+++ spec/requests/api/v1/meal_foods/meal_foods_delete_spec.rb
@@ -42,4 +42,26 @@ describe 'delete /api/v1/meals/:meal_id/foods/:id' do
     new_meal = JSON.parse(response.body, symbolize_names: true)
     expect(new_meal).to eq(new_expectation)
   end
+
+  it 'should return a 404 if the food does not exist' do
+    meal1 = Meal.create(name: 'Breakfast')
+    food1 = Food.create(name: 'Banana', calories: 150)
+    MealFood.create(meal_id: meal1.id, food_id: food1.id)
+    nonexistent_id = 1000000
+
+    delete "/api/v1/meals/#{meal1.id}/foods/#{nonexistent_id}"
+
+    expect(response.status).to eq(404)
+  end
+
+  it 'should return a 404 if the meal does not exist' do
+    meal1 = Meal.create(name: 'Breakfast')
+    food1 = Food.create(name: 'Banana', calories: 150)
+    MealFood.create(meal_id: meal1.id, food_id: food1.id)
+    nonexistent_id = 1000000
+
+    delete "/api/v1/meals/#{nonexistent_id}/foods/#{food1.id}"
+
+    expect(response.status).to eq(404)
+  end
 end
